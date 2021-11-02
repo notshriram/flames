f131b24411ae1b7c62a2d153833b8d9c9b7ddc11
aribami <20951a0504@iare.ac.in>
2021-11-02 18:50:03 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let loadNames = () => {
+  let name1 = document.getElementById("Name-1").value;
+  let name2 = document.getElementById("Name-2").value;
+  document.getElementById("Name1big").innerHTML = name1;
+  document.getElementById("Name2big").innerHTML = name2;
+};
